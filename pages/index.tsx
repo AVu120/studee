2b589a4c2536ce0f11201a4149daafb672ffd06f
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-03-28 21:17:15 +1100

    Enable auto import sorting via eslint.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+
 import styles from "../styles/Home.module.css";
+import connectToDatabase from "../utils/database/mongodb";
 
 const Home: NextPage = () => (
   <div className={styles.container}>
@@ -66,3 +68,23 @@ const Home: NextPage = () => (
 );
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  try {
+    // client.db() will be the default database passed in the MONGODB_URI
+    // You can change the database by calling the client.db() function and specifying a database like:
+    // const db = client.db("myDatabase");
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+    await connectToDatabase();
+    return {
+      props: {},
+    };
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return {
+      props: {},
+    };
+  }
+};
