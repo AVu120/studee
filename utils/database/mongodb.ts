4c022cd725ac3316e391c64d36c1f2d2a9a33fa5
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-03-28 21:06:18 +1100

    Remove typescript warnings.

---
+++ utils/database/mongodb.ts
@@ -0,0 +1,42 @@
+import { MongoClient } from "mongodb";
+import type { MongoClientOptions, Db } from "mongodb";
+
+const { MONGODB_URI } = process.env;
+
+// check the MongoDB URI
+if (!MONGODB_URI) {
+  throw new Error("Define the MONGODB_URI environmental variable");
+}
+
+let cachedClient: MongoClient | null = null;
+let cachedDb: Db | null = null;
+
+const connectToDatabase = async () => {
+  // check the cached.
+  if (cachedClient && cachedDb) {
+    // load from cache
+    return {
+      client: cachedClient,
+      db: cachedDb,
+    };
+  }
+
+  // Set the connection options
+  const opts: MongoClientOptions = {};
+
+  // Connect to cluster
+  const client = new MongoClient(MONGODB_URI, opts);
+  await client.connect();
+  const db = client.db();
+
+  // set cache
+  cachedClient = client;
+  cachedDb = db;
+
+  return {
+    client: cachedClient,
+    db: cachedDb,
+  };
+};
+
+export default connectToDatabase;
