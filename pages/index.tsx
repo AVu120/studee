a300304fcf772b697aa2428644a633087d32c7d7
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-04-03 13:45:42 +1000

    Move css module file into appropriate folder.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,11 @@ import {
 } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import type { ChangeEvent } from "react";
 import { useState } from "react";
+import styles from "styles/pages/Home.module.scss";
 import passwordSchema from "utils/validators/password";
 
-import styles from "../styles/Home.module.scss";
 import connectToDatabase from "../utils/database/mongodb";
 
 const Home: NextPage = () => {
