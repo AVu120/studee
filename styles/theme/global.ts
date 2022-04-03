43949a3a6a38d5c362c4a215920cd538a5fe4b05
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-04-03 13:35:28 +1000

    Add global config to chakra-ui theme.

---
+++ styles/theme/global.ts
@@ -0,0 +1,12 @@
+export default {
+  global: {
+    "html, body": {
+      color: "gray.600",
+      lineHeight: "tall",
+    },
+    a: {
+      color: "teal.500",
+    },
+    h1: { fontSize: "3rem" },
+  },
+};
--- styles/theme/index.ts
+++ styles/theme/index.ts
@@ -1,11 +1,12 @@
 import { extendTheme } from "@chakra-ui/react";
 import colors from "styles/theme/colors";
 import Button from "styles/theme/components/button";
+import global from "styles/theme/global";
 
 const components = {
   Button,
 };
 
-const myTheme = extendTheme({ colors, components });
+const myTheme = extendTheme({ colors, components, styles: global });
 
 export default myTheme;
