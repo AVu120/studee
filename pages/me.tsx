62400fd00c0878fbfd45b0bbfee4ae8be36e8417
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-04-15 16:38:14 +1000

    Fix TS error.

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@chakra-ui/react";
-import type { NextPage, NextPageContext } from "next";
+import type { GetServerSideProps, NextPage, NextPageContext } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useState } from "react";
