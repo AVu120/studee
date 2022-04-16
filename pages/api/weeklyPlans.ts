1f89ec57b5e09fffde0943865c570a697f8f3379
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-04-16 13:28:04 +1000

    add GET weeklyPlans API.

--- pages/api/weeklyPlans.ts
+++ pages/api/weeklyPlans.ts
@@ -2,15 +2,41 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from "server-utils/database/mongodb";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (!req.body) {
-    return res.status(400).json({ message: "No body" });
-  }
+  try {
+    if (req.method === "GET") {
+      if (!req.query) {
+        return res.status(400).json({ message: "No query params" });
+      }
 
-  if (req.method === "PUT") {
-    try {
+      const { userId, startDate } = req.query;
+      if (!userId || !startDate) {
+        return res
+          .status(400)
+          .json({ message: `No ${!userId ? "userId" : "startDate"}` });
+      }
+
       const { db } = await connectToDatabase();
+      const weeklyPlan = await db
+        .collection("weeklyPlans")
+        .findOne({ userId, startDate });
+      return weeklyPlan
+        ? res.status(200).json(weeklyPlan)
+        : res.status(404).json("Not found");
+    }
 
+    if (req.method === "PUT") {
+      if (!req.body) {
+        return res.status(400).json({ message: "No body" });
+      }
       const { userId, startDate, ...payload } = req.body;
+
+      if (!userId || !startDate) {
+        return res
+          .status(400)
+          .json({ message: `No ${!userId ? "userId" : "startDate"}` });
+      }
+
+      const { db } = await connectToDatabase();
 
       const query = { userId, startDate };
       const update = { $set: payload };
@@ -18,9 +44,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
       await db.collection(`weeklyPlans`).updateOne(query, update, options);
       return res.status(200).json({ message: "Update successful" });
-    } catch (error: any) {
-      return res.status(500).json({ message: error.message });
     }
+    return res.status(404).json({ message: "Not found" });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
   }
-  return res.status(404).json({ message: "Not found" });
 };
