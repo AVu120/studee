a35d13389d718d4c81b19ac4dc093060b129e01c
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-04-30 09:56:08 +1000

    Add google analytics script.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,33 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import myTheme from "styles/theme/index";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={myTheme}>
-      {/* // eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload" id="google-analytics-script">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+
+      <ChakraProvider theme={myTheme}>
+        {/* // eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
