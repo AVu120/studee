b8a2c6561129ce56320d86ea0637333046bd37de
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-04-30 10:51:07 +1000

    Add next webpack bundle analyzer.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
-}
+});
 
 module.exports = nextConfig
